3d65514a155e2d8f3698b0042845c1436c61e3ee
padmapriya-26 <padmapriyadevarapalli2607@gmail.com>
2025-11-20 12:22:11 +0530

    Create padma.groovy

---
+++ padma.groovy
@@ -0,0 +1,20 @@
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+         steps {
+            echo "welcome maven"
+         }
+        }
+         stage('test') {
+            steps {
+            echo "welcome to test"
+            }
+         }
+         stage('sonar') {
+            steps {
+            echo "welcome to sonar"
+            }
+         }
+        }
+    }
